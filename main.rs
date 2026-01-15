3be7c38d6bbbf16fbd3ecd93af330d49e54a7c5a
Harikrishnan R <harikrishnanr0505@gmail.com>
2026-01-15 20:11:32 +0530

    lexer

--- main.rs
+++ main.rs
@@ -11,33 +11,6 @@ use std::process::Command;
 
 mod lexer;
 
-// fn assemble_and_link() -> Result<(), String> {
-//     let nasm_output = Command::new("nasm")
-//         .args(&["-f", "elf64", "output.asm", "-o", "output.o"])
-//         .output()
-//         .map_err(|e| format!("Failed to run nasm: {}", e))?;
-
-//     if !nasm_output.status.success() {
-//         return Err(format!(
-//             "NASM assembly failed:\n{}",
-//             String::from_utf8_lossy(&nasm_output.stderr)
-//         ));
-//     }
-
-//     let ld_output = Command::new("ld")
-//         .args(&["output.o", "-o", "output"])
-//         .output()
-//         .map_err(|e| format!("Failed to run ld: {}", e))?;
-
-//     if !ld_output.status.success() {
-//         return Err(format!(
-//             "Linking failed:\n{}",
-//             String::from_utf8_lossy(&ld_output.stderr)
-//         ));
-//     }
-//     Ok(())
-// }
-
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -65,29 +38,8 @@ fn main() {
     };
 
     let tokens: Vec<Token> = lexer::tokenize(&contents);
-    println!("{:?}", tokens)
-    // let tree: Result<Program, String> = parser::parse(tokens);
-    // let res: Result<Program, String> = semanter::analyze(tree);
-
-    // match res {
-    //     Ok(program) => {
-    //         let code: String = codegen::generate(&program);
-    //         fs::write("output.asm", code).expect("Failed to write assembly");
-    //     }
-
-    //     Err(e) => {
-    //         eprintln!("Error: {}", e);
-    //         process::exit(1);
-    //     }
-    // }
-
-    // match assemble_and_link() {
-    //     Ok(()) => {
-    //         // pass
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Compilation failed: {}", e);
-    //         process::exit(1);
-    //     }
-    // }
+    let n: usize = tokens.len();
+    for i in 1..n {
+        println!("{:?}", tokens[i]);
+    }
 }

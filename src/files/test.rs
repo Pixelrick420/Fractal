da369a081c64dd49ef691761a2ab223fd5456aa4
Theerttha <theerttha.s@gmail.com>
2026-03-17 01:17:24 +0530

    remove rs files

--- src/files/test.rs
+++
@@ -1,8 +0,0 @@
-#![allow(unused_variables, unused_mut, dead_code, non_snake_case, unused_imports, unreachable_patterns)]
-use std::io::{self, BufRead, Write};
-
-fn main() {
-    let mut x: i64 = 10_i64;
-    let mut y: f64 = 8.0_f64;
-    let mut z: i64 = (x + (y as i64));
-}
